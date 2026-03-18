e9ba750c0e3a11002b0a82c54de025f0dcba5f79
jairogarcia1 <jairo-123@live.com>
2026-03-18 15:23:54 -0700

    Completed Day5 Modules 3 of 4

--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,2 +1,28 @@
-student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
-print(range(1, 10))
+#student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+#print(range(1, 10))
+
+
+#max()
+"""max = 0
+for score in student_scores:
+    if score > max:
+        max = score
+print(f"The highest score in the class is: {max}")"""
+
+#min
+"""min=200
+for score in student_scores:
+    if score < min:
+        min = score
+print(f"The lowest score in the class is: {min}")"""
+
+# find max
+student_scores = [8, 65, 89, 55, 91, 64, 89]
+
+max = 0
+
+for score in student_scores:
+    if score > max:
+        max = score
+print(f"The highest score in the class is: {max}")
+
--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -7,4 +7,9 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+qletter = letters[random.randint(0, len(letters) - 1)]
+qnumber = numbers[random.randint(0, len(numbers) - 1)]
+qsymbol = symbols[random.randint(0, len(symbols) - 1)]
+
+print(qletter, qnumber, qsymbol)
 
