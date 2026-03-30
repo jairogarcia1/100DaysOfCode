62937c6f604a025f62985685160f56bb32ea6f0d
Jairo Garcia <jairo-123@live.com>
2026-03-29 20:01:09 -0700

    Completed Day 8 2/5

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,6 @@
+def greet():
+    print("Hello World")
+    print("How are you?")
+    print("How are you doing?")
+
+greet()
--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,24 @@
 # Functions with input
 
-def greet_with_name(name):
+"""def greet_with_name(name):
     print(f"Hello {name}")
     print(f"How do you do {name}?")
 
 
-greet_with_name("Jack Bauer")
+greet_with_name("Jack Bauer")"""
+
+
+"""
+def force(mass,acceleration):
+    force = mass*acceleration
+    print (force)
+force(6,5)
+"""
+
+
+def greet_with(name, location):
+    print(f"Hello {name},")
+    print(f"What is it like in {location}.")
+
+
+greet_with("Jack Bauer", "Seattle")
