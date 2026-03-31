e91ecc94982913d62382c86a8ffdc832185a69cb
Jairo Garcia <jairo-123@live.com>
2026-03-30 22:01:22 -0700

    Completed Day 8 3/5

--- Day 8/Caesar Cipher 1/main.py
+++ Day 8/Caesar Cipher 1/main.py
@@ -1,4 +1,6 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from github.PublicKey import encrypt
+
+alphabet = [' ','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
@@ -6,6 +8,15 @@ shift = int(input("Type the shift number:\n"))
 
 
 # TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+def encrypt():
+    encrypted_text = ""
+    for char in text:
+        if char in alphabet:
+            encrypted_text = alphabet[alphabet.index(char) - shift]
+            print(encrypted_text)
+        else:
+            print(f"'{char}' is not a letter. It will be added to the encrypted text without being shifted.")
+encrypt()
 
 # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
 #  by the shift amount and print the encrypted text.
