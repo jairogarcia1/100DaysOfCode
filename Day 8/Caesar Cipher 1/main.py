64a11f1e0edffc3095dd7bfd767f7567b60b111e
jairogarcia1 <jairo-123@live.com>
2026-04-01 15:16:36 -0700

    Completed Day8 3/5 encoding

--- Day 8/Caesar Cipher 1/main.py
+++ Day 8/Caesar Cipher 1/main.py
@@ -1,30 +1,25 @@
-from github.PublicKey import encrypt
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-alphabet = [' ','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+"""direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+shift = int(input("Type the shift number:\n"))"""
 
 
 # TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
 # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
 #  by the shift amount and print the encrypted text.
+# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
+#  message.
 
 def encrypt(original_text, shift_amount):
+    cypher_text = ""
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) + shift_amount
+        cypher_text += alphabet[shifted_position % len(alphabet)]
+    print(f"Here is the encoded result: {cypher_text}")
 
-    for char in original_text:
-        if char in alphabet:
-            position = alphabet.index(char)
-            new_position = (position + shift_amount) % 26
-            new_char = alphabet[new_position]
-    print(f"here is the encoded result: '{new_char}'.")
-        else:
-            print(f"'{char}' is not a letter. It will be added to the encrypted text without being shifted.")
-encrypt("hello", 2)
+encrypt("z", 9)
 
 # TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
 
-# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
-#  message.
 
