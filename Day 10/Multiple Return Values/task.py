653691b50323fe51887844b29fb43afb8cc142e3
Jairo Garcia <jairo-123@live.com>
2026-04-10 23:22:39 -0700

    Completed Day 10 2/4

--- Day 10/Multiple Return Values/task.py
+++ Day 10/Multiple Return Values/task.py
@@ -1,7 +1,9 @@
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You did not provide both the first and last name"
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
 
 
-print(format_name("AnGEla", "YU"))
+print(format_name(input("Enter your first name:\n "), input("Enter your last name:\n ")))
