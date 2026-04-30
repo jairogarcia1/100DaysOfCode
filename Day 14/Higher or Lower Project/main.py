5f0725a04e37bec65fa1e0b2b9dbd4b5f6823765
Jairo Garcia <jairo-123@live.com>
2026-04-29 22:23:26 -0700

    Completed Day14

--- Day 14/Higher or Lower Project/main.py
+++ Day 14/Higher or Lower Project/main.py
@@ -1,24 +1,60 @@
-from random import randint
+from art import logo, vs
 from game_data import data
-from art import *
+import random
 
-choice1 = randint(0, 49)
-choice2 = randint(0, 49)
+def format_data(account):
+    """Format the account data into printable format"""
+    account_name = account["name"]
+    account_description = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_description}, from {account_country}"
 
-print(f"Compare A: {data[choice1]['name']}, a {data[choice1]['description']} from {data[choice1]['country']}")
-print(f"Against B: {data[choice2]['name']}, a {data[choice2]['description']} from {data[choice2]['country']}")
+def check_answer(user_guess, a_followers, b_followers):
+    """Checks followers against user's guess and returns if they got it right."""
+    if a_followers > b_followers:
+        return user_guess == "a"
+    else:
+        return user_guess == "b"
 
-answer = input("Who has more followers? Type 'A' or 'B': ").lower()
 
+# Display art
+print(logo)
 score = 0
+game_should_continue = True
+account_b  = random.choice(data)
 
-if answer == 'a':
-    if data[choice1]['follower_count'] > data[choice2]['follower_count']:
-        print(f"You are right! Current score: {score + 1}.")
+# Make the game repeatable
+while game_should_continue:
+    # Generate a random account from the game data
+    # Making account at position B become the next account at position A.
+    account_a = account_b
+    account_b = random.choice(data)
+    if account_a == account_b:
+        account_b =random.choice(data)
+
+    # Format the account data into orintable format.
+    print(f"Compare A: {format_data(account_a)}")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}")
+
+    # Ask user for a guess
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    # clear the screen
+    print("\n"*20)
+    print(logo)
+
+    # check if user is correct
+    ## Get follower count of each account
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    # Give user feedback on their guess
+    # Score keeping
+    if is_correct:
+        score += 1
+        print(f"You are right! Current score: {score}.")
     else:
         print(f"Sorry, that's wrong. Final score: {score}.")
-elif answer == 'b':
-    if data[choice2]['follower_count'] > data[choice1]['follower_count']:
-        print(f"You are right! Current score: {score + 1}.")
-    else:
-        print(f"Sorry, that's wrong.  Final score: {score}.")
+        game_should_continue = False
