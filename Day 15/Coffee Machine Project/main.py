847bdaa428117dc6df440cfe7d38b6605946b02c
jairogarcia1 <jairo-123@live.com>
2026-05-04 13:52:56 -0700

    adding prompt1 in Day15

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -30,3 +30,24 @@ resources = {
     "coffee": 100,
 }
 
+"""
+1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
+a. Check the user’s input to decide what to do next.
+b. The prompt should show every time action has completed, e.g. once the drink is
+dispensed. The prompt should show again to serve the next customer
+"""
+while resources["water"] > 0 and resources["milk"] > 0 and resources["coffee"] > 0:
+    order = input("What would you like? (espresso/latte/cappuccino):").lower()
+    if order == "espresso":
+        choice = MENU["espresso"]
+        resources["water"] -= choice["ingredients"]["water"]
+        resources["coffee"] -= choice["ingredients"]["coffee"]
+        print(f"{resources['water']} water, {resources['coffee']} coffee")
+    elif order == "latte":
+        choice = MENU["latte"]
+    elif order == "cappuccino":
+        choice = MENU["cappuccino"]
+    else:
+        print("Invalid choice.")
+        exit()
+
